6f11a099382f1b66a664a5d1ae46323a1081a25d
Rendi Supriatna <supriatnarendi@gmail.com>
2015-09-16 22:18:06 +0700

    tah bro

---
+++ js/main.js
@@ -0,0 +1,37 @@
+	new WOW().init();
+
+     var data_filter =  $('#sl2').slider({
+
+     })
+        .on('slide',function(){
+        	console.log(data_filter.value)
+        	 var isotop_filter = $('.grid').isotope({
+				  // options
+				  itemSelector: '.grid-item',
+				  layoutMode: 'fitRows',
+
+				  filter: function() {
+				  		// console.log(data_filter.value[0])
+					  	var number = $(this).data('price');
+					  	var number = $(this).attr('data-price');
+					  	return parseInt( number, 10 ) >= data_filter.value[0] && parseInt( number, 10 ) <= data_filter.value[1];
+					} 
+				});
+        })
+        .data('slider');
+
+	var width = $(window).width(), height = $(window).height();
+	if (width > 767) {
+		$('.dropdown').hover(function(){
+			$(this).addClass('open')
+		},function(){
+			$(this).removeClass('open')
+		})
+	}else if (width > 767) {
+		$('.navbar').removeClass('navbar-fixed-top')
+	}
+		$('.logo').hover(function(){
+			$(this).addClass('animated').addClass('bounce')
+		},function(){
+			$(this).removeClass('animated').removeClass('bounce')
+		})
